36223e91bdd722322ca17575297c7c350295e5bc
natalelea <74679548+natalelea@users.noreply.github.com>
2023-03-08 22:07:15 +0100

    String Parsing
    Add String Parsing script

--- HW_1/String_Parsing.sql
+++ HW_1/String_Parsing.sql
@@ -1,4 +1,3 @@
-
 WITH json_string AS
 	(
 		SELECT '[{"employee_id": "5181816516151", "department_id": "1", "class": "src\bin\comp\json"}, {"employee_id": "925155", "department_id": "1", "class": "src\bin\comp\json"}, {"employee_id": "815153", "department_id": "2", "class": "src\bin\comp\json"}, {"employee_id": "967", "department_id": "", "class": "src\bin\comp\json"}]' [str]
